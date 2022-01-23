f6b25a6d735abdd50d36e95e371ef20f27aa20ab
ifenega <69639702+ifenega@users.noreply.github.com>
2022-01-23 15:33:42 +0100

    slider
    i added a slider
    not yet responsive and cannot work with time

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
+const sliderInfo = [
+    {
+        id : 0,
+        text: "Increase your Conversion rate +35%"
+    },
+    {
+        id : 1,
+        text: "Increase repeat purchases"
+    },
+    {
+        id : 2,
+        text: "Protect your working capital"
+    }
+]
+
+const textContainer = document.getElementById('text-container')
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+
 const toggle = document.querySelector('.toggle')
 const navigation = document.querySelector('.navigation')
 
 toggle.addEventListener('click', () => {
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
+    navigation.classList.toggle('active')
+    toggle.classList.toggle('active')
     
     
 })
+
+
+//slider
+//set starting item
+let currentItem = 0
+
+//load initial item 
+window.addEventListener("DOMContentLoaded", ()=> {
+    changeText(currentItem)
+})
+
+const changeText = () => {
+    const item = sliderInfo[currentItem]
+    textContainer.textContent = item.text
+}
+
+nextBtn.addEventListener('click', ()=>{
+    currentItem++
+    if(currentItem > sliderInfo.length-1) {
+        currentItem = 0
+    }
+    changeText(currentItem)
+})
+
+prevBtn.addEventListener('click', ()=>{
+    currentItem--
+    if(currentItem < 0 ) {
+        currentItem = sliderInfo.length-1
+    }
+    changeText(currentItem)
+})
+
+
+
+
